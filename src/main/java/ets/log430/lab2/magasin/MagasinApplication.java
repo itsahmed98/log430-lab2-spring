78fe7a92e6a8243e2a87763e8171ef4a25522c52
itsahmed98 <ahmed.akram98@yahoo.ca>
2025-05-28 08:51:23 -0400

    initialize project

---
+++ src/main/java/ets/log430/lab2/magasin/MagasinApplication.java
@@ -0,0 +1,13 @@
+package ets.log430.lab2.magasin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagasinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagasinApplication.class, args);
+	}
+
+}
